731ffc96a081fef6ae8675837cdc1f841d69bc70
Luke Kelley <125288953+lucasjkelley@users.noreply.github.com>
2023-04-11 16:46:36 -0500

    implemented EF and updated program

---
+++ BorderlandsStore.DATA.EF/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BorderlandsStore.DATA.EF.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Weapons = new HashSet<Weapon>();
+        }
+
+        public int CategoryId { get; set; }
+        public string Category1 { get; set; } = null!;
+        public string CategoryDescription { get; set; } = null!;
+
+        public virtual ICollection<Weapon> Weapons { get; set; }
+    }
+}
